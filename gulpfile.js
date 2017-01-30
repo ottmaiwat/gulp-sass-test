2673a98f017a14ac8e937883051a9fb7456d9947
ott maiwat <ott.maiwat@vml.com>
2017-01-30 14:42:37 -0600

    Fix gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// Modules
+var gulp = require('gulp'),
+    autoprefixerModule = require('gulp-autoprefixer'),
+    sassModule = require('gulp-sass'),
+    sourcemapsModule = require('gulp-sourcemaps');
+
+
+// Directories
+var sassSrcFolder = '/scss/*.scss',
+    sassDestFolder = '/css';
+
+
+var sassTranspileOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+
+gulp.task('sass', function(){
+    return gulp
+        .src(sassSrcFolder)
+        .pipe(sourcemapsModule.init())
+        .pipe(sassModule(sassTranspileOptions).on('error', sassModule.logError))
+        .pipe(sourcemapsModule.write('.'))
+        .pipe(gulp.dest(sassDestFolder));
+});
+
+
+gulp.task('default', ['sass'], function(){});
